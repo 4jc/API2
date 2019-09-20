edfefacb3b8b803b1f5d6c0ab22e841b4e90cb1c
4jc <josec10@cox.net>
2019-09-19 17:21:43 -0700

    first commit

---
+++ src/main/java/com/promineotech/inventoryManagement/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.promineotech.inventoryManagement.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.promineotech.inventoryManagement.entity.Customer;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long>{}
---
+++ src/main/java/com/promineotech/inventoryManagement/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.promineotech.inventoryManagement.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.promineotech.inventoryManagement.entity.Product;
+
+public interface ProductRepository extends CrudRepository<Product, Long>{}
